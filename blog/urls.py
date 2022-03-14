95b298d2c4de84db0dee03e78abdfefcb61fd3f8
Elmer Estrada <elmerestrada27@gmail.com>
2022-03-14 00:43:38 -0300

    Se agregaron cambios de paginacion

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from sre_constants import IN
 from django.contrib import admin
-from homepage.views import IndexView
+from homepage.views import IndexView,AboutView
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
+    path('about/', AboutView.as_view(), name='about'),
     path('post/', include(('posts.urls'), namespace='post'))    # namespace funciona si se define el nobmre de app, en el archivo urls.py de la App
 ]
